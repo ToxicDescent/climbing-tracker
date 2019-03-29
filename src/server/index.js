35f11ba01d6ccc36282fe1af2187740493e2b719
Patrick Haikal <patrick.haikal@investorist.com>
2019-03-29 16:24:23 +1100

    Added cors support to the server

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,14 @@
-import express from 'express';
 import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
+
+app.get('/', (request, response) => {
+  response.send('Hello World!');
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listeng on port: ${process.env.PORT}`);
