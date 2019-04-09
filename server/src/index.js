49ad5108ccf9e1a3f6ee36c20200c83440f5a1f6
Patrick Haikal <patrick.haikal@investorist.com>
2019-04-10 09:47:48 +1000

    Fixed some bugs in the server build

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import 'regenerator-runtime';
 
 import models, { connectDb } from './models';
 import routes from './routes';
