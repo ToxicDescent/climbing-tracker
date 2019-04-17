df62dc09301cc4b7218940cb484e190c3f351eec
Patrick Haikal <pathaikal@gmail.com>
2019-04-17 22:29:36 +1000

    Added a connection to my running server

--- client/src/environment/environment.js
+++ client/src/environment/environment.js
@@ -1,2 +1,7 @@
 // eslint-disable-next-line import/prefer-default-export
-export const getServerUrl = () => 'http://localhost:9000';
+export const getServerUrl = () => {
+  if (process.env.NODE_ENV === 'development') {
+    return 'http://localhost:9000';
+  }
+  return 'http://175.32.42.210:9000';
+};
