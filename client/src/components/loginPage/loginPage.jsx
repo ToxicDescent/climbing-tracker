6184783376ea65461cfec4e78556818154048959
Patrick Haikal <patrick.haikal@investorist.com>
2019-04-29 11:05:09 +1000

    Added the login page code

--- client/src/components/loginPage/loginPage.jsx
+++ client/src/components/loginPage/loginPage.jsx
@@ -1,5 +1,52 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
+import styles from './loginPage.css';
+
 const LoginPage = () => {
-  return null;
+  return (
+    <div className={styles.loginPage}>
+      <Paper className={styles.paper}>
+        <Avatar className={styles.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <form>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="email">Email</InputLabel>
+            <Input id="email" name="email" autoComplete="email" autoFocus />
+          </FormControl>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="password"
+            />
+          </FormControl>
+          <Button
+            className={styles.submit}
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+          >
+            Login
+          </Button>
+        </form>
+      </Paper>
+    </div>
+  );
 };
 
 export default LoginPage;
