3b45915117c5587e246e4adec371ac8c2c328872
Patrick Haikal <patrick.haikal@investorist.com>
2019-05-01 11:16:04 +1000

    Improved the user validation

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: true },
-  hash: { type: String, required: true },
   name: { type: String, required: true },
+  hash: { type: String, required: true },
   createdDate: { type: Date, default: Date.now }
 });
 
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,46 +1,62 @@
 import { Router } from 'express';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 import { wrapAsync } from '../middleware/wrapAsync';
-import { authenticateUser } from '../controllers/user';
 import { createError } from '../utility/createError';
 import models from '../models';
 
 const router = Router();
 
-router.post(
-  '/login',
-  wrapAsync(async (request, response) => {
-    const authenticatedUser = await authenticateUser(request.body);
-    if (authenticatedUser) {
-      response.json(authenticatedUser);
-    }
-    throw createError('Email or password is incorrect', 400);
-  })
-);
-
 const getUserByEmail = async email => {
   return await models.User.findOne({ email }).select('-hash');
 };
+const getUserByEmailWithHash = async email => {
+  return await models.User.findOne({ email });
+};
+
+router.post(
+  '/login',
+  wrapAsync(async (request, response) => {
+    // find user via email
+    const userWithHash = await getUserByEmailWithHash(request.body.email);
+    // validate password
+    if (
+      !userWithHash ||
+      !bcrypt.compareSync(request.body.password, userWithHash.hash)
+    ) {
+      throw createError('Email or password is incorrect', 400);
+    }
+    const { hash, ...userWithoutHash } = userWithHash.toObject();
+    // create jwt
+    jwt.sign(
+      { sub: userWithHash.id },
+      'this is a secure secret',
+      (error, token) => {
+        if (error) {
+          throw createError('Failed to sign token', 400);
+        }
+        response.json({ ...userWithoutHash, token });
+      }
+    );
+  })
+);
 
 router.post(
   '/signup',
   wrapAsync(async (request, response) => {
-    // validate
+    // validate doesn't exist
     if (await getUserByEmail(request.body.email)) {
       throw createError(
         `Email (${request.body.email}) is registered to another user`,
         400
       );
     }
-
     const user = new models.User(request.body);
-
     // hash password
     if (request.body.password) {
       user.hash = bcrypt.hashSync(request.body.password, 10);
     }
-
     // save user
     await user.save();
   })
